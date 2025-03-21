92799e1478ed99f51b9fbfb32f8146b329fdc0a9
AgishevDM <109414135+AgishevDm@users.noreply.github.com>
2025-03-21 16:33:45 +0300

    Исходники к LR_9_DevOps

---
+++ docker9compose/flask-redis/app.py
@@ -0,0 +1,17 @@
+import time
+import redis
+from flask import Flask
+
+app = Flask(__name__)
+cache = redis.Redis(host='redis', port=6379)
+
+def get_hit_count():
+    return cache.incr('hits')
+
+@app.route('/')
+def hello():
+    count = get_hit_count()
+    return f'Hello World! I have been seen {count} times.\n'
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
